a8c1036f90b2e61d5e7a5c09d85f8fc305fae7c0
Alejandro Colmenarejo <a.colmenarejo@usp.ceu.es>
2019-11-07 18:12:26 +0100

    Inicio de App y boton a nueva Actividad

---
+++ app/src/main/java/com/example/erasmusapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.erasmusapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        VideoView videoView=(VideoView)findViewById(R.id.videoView1);
+        MediaController media=new MediaController(this);
+
+        media.setAnchorView(videoView);
+
+        Uri uri =Uri.parse("android.resource://com.example.myapplication1/" + R.raw.logonosound1);
+
+        videoView.setMediaController(media);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+
+
+    }
+
+    public void Siguiente(View view){
+
+        Intent siguiente = new Intent(this,Menu.class);
+        startActivity(siguiente);
+
+    }
+
+
+
+
+}
